93cd78ffae1326ad019ed1d28dab62e68e37b2a0
abolfazl <134769576+abolfazlsh07@users.noreply.github.com>
2024-10-05 18:06:39 +0330

    Update watermelon_melon.py

--- Watermelon_melon/watermelon_melon.py
+++ Watermelon_melon/watermelon_melon.py
@@ -1,9 +1,9 @@
 h = int(input())
 k = int(input())
 
-# بررسی اینکه آیا مجموع 2h + k زوج است
+# Check if the sum of 2h + k is even
 if (2 * h + k) % 2 == 0:
-    # بررسی امکان تقسیم برابر در شرایط خاص
+   # Investigating the possibility of equal division in certain circumstances
     if h == 0 and k % 2 != 0:
         print("NO")
     elif k == 0 and h % 2 != 0:
